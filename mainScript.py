95c064c687dba710fd0e120c28d35bb293c5c71e
antoinevastel <antoine.vastel@gmail.com>
2015-06-11 18:14:37 +0200

    tested to get localStorage data

--- mainScript.py
+++ mainScript.py
@@ -1,10 +1,21 @@
 __author__ = 'avastel'
 # -*- coding: utf-8 -*-
 from Firefox import *
+import sqlite3 as lite
 
 def main():
     firefox = Firefox()
     firefox.run()
+
+    # con = lite.connect('/home/avastel/.mozilla/firefox/kwdl11go.default/webappsstore.sqlite')
+    #
+    # cur = con.cursor()
+    # cur.execute('select * from webappsstore2 where key ="tempFpContent"')
+    #
+    # rows = cur.fetchall()
+    #
+    # for row in rows:
+    #     print row
 
 if __name__ == '__main__':
     main()
